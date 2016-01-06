6593e7dd3cffb54c7cddf4b7fda65f8a3e95f80f
Nexus64 <pedro9romero4gonzalez@gmail.com>
2016-01-06 01:21:51 +0100

    Ball with basic movement added

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 direction = Vector3.up + Vector3.forward;
+		GetComponent<Rigidbody> ().velocity = direction.normalized * speed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 direction = GetComponent<Rigidbody> ().velocity.normalized;
+		GetComponent<Rigidbody> ().velocity = direction * speed;
+	}
+}
